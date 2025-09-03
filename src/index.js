753920b1723b878e8858dec36314d2c01bc5ef11
sejong.lee <hhh3915@gmail.com>
2025-09-03 14:38:56 +0900

    second

--- src/index.js
+++ src/index.js
@@ -7,10 +7,16 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename='react-netflix'>
     <App />
   </BrowserRouter>
 );
+
+// ReactDOM.render(
+//   <BrowserRouter basename='react-netflix'>
+//     <App />
+//   </BrowserRouter>
+// )
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
